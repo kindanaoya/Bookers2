72a297596715548d4692ebedf5c53a18fcdad3fd
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-08-30 07:17:55 +0000

    [Add]book(index,create,show,update,destroy)&user(show,edit)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,27 +9,38 @@ class BooksController < ApplicationController
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def index
     @book = Book.new
     @books = Book.all
-    @book.user_id = current_user.id
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
    private
 
   def book_params
     params.require(:book).permit(:title, :image, :body)
   end
-  
+
 end
