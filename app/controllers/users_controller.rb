6ac8a0710b92beb756d9673a8dcf0d33466bfc91
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-08-30 15:40:08 +0000

    [Update]レイアウトの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-   
+
   end
 
   def show
@@ -11,17 +11,17 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to book_path(currentuser.id)
+    redirect_to new_book_path
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
